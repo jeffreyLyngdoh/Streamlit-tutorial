1aeb186ad0de648bbd20a0e050de025dbf3f97b1
jeffreyLyngdoh <jeffrey.k.lyngdoh@gmail.com>
2024-10-12 16:15:50 -0400

    load model

--- app.py
+++ app.py
@@ -11,6 +11,16 @@ from PIL import Image
 
 st.title('This is just a test made a change')
 
+# Try to load the model and handle any errors
+model = None
+try:
+    model = load_model('./Image_Classification_Tutorial.keras')
+    st.write("Model loaded successfully")
+except Exception as e:
+    st.write(f"Error loading model: {e}")
+
+
+
 uploaded_file = st.file_uploader("Choose an image...", type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
